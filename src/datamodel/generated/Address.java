b13c0f11665a4b4cf48fe22839c80354883a8260
Levent avgören <leventavgoren@gmail.com>
2023-07-01 22:57:17 +0200

    change interface Address.java to class

--- src/datamodel/generated/Address.java
+++ src/datamodel/generated/Address.java
@@ -13,41 +13,78 @@ package datamodel.generated;
  * @author <code style=color:blue>{@value application.package_info#Author}</code>
  */
 
-public interface Address {
+public class Address {
 
     /**
      * Enum to represent country codes, e.g. "D" for Germany, "AU" for Austria, etc.
      *
      */
-    enum Country { D, AU, CH, NL, GB, IT, SP, USA };
+    private final Country country;
+    /**
+     * 4 or 5 digit postal routing number (Postleitzahl).
+     */
+    private final String zip;
+    /**
+     * Name of a city, town or village as used in a postal address.
+     */
+    private final String city;
+    /**
+     * Name of the street including street number.
+     */
+    private final String street;
 
+
+    enum Country { 
+        D, AU, CH, NL, GB, IT, SP, USA
+    };
+
+    public Address(String zip, String city, String street) {
+        this.country=null;
+        this.zip=zip;
+        this.city=city;
+        this.street=street;
+    }
+    public Address(String country, String zip, String city, String street) {
+        this.country=null;
+        this.zip=zip;
+        this.city=city;
+        this.street=street;
+    }
     /**
      * Return country code.
      * 
      * @return country code.
      */
-    public Country getCountry();
+    public Country getCountry(){
+        return country;
+    }
 
     /**
      * Return zip code (Postleitzahl).
      * 
      * @return zip code.
      */
-    public String getZip();
+    public String getZip(){
+        return zip;
+    }
 
     /**
      * Return name of city, town or village as used in a postal address.
      * 
      * @return name of city, town or village.
      */
-    public String getCity();
+    public String getCity(){
+        return city;
+    }
 
     /**
      * Return name of the street with house number.
      * 
      * @return name of the street with house number.
      */
-    public String getStreet();
+    public String getStreet(){
+        return street;
+    }
 
 }
 
